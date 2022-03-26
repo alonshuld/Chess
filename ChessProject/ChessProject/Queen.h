8012199ca9de56d6fa2d52b90f4b26d9fc9a0acd
Shiraz Shiraz <shirazamos2@gmail.com>
2022-03-26 23:39:49 +0300

    added project for cr

--- ChessProject/ChessProject/Queen.h
+++ ChessProject/ChessProject/Queen.h
@@ -8,4 +8,5 @@ public:
 	~Queen();
 	void isValidMove(const string cords, const string* board, const bool currentColor) override;
 	void checkIfNotRunOver(const int startX, const int startY, const char x, const char y, const string* board);
+
 };
